5d4264c9e42cb72e178661dc103adf952a7d3d6c
Viktor Diezel <viktor.diezel@posteo.de>
2023-01-09 23:57:48 +0100

    removes markdown files

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -2,7 +2,7 @@
 --  set background=light
 --]])
 
-local colorscheme = "gruvbox"
+local colorscheme = "tokyonight"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
