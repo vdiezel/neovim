6d5fbdf30410bcf53b621a911420404ac915ebf9
Viktor Diezel <viktor.diezel@posteo.de>
2023-01-13 09:14:15 +0100

    update colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -2,7 +2,7 @@
 --  set background=light
 --]])
 
-local colorscheme = "tokyonight"
+local colorscheme = "gruvbox"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
